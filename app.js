97d8114c06b40bdb82893c5e5a84f5aae9366511
andreassilvas <andreassilvas@git.com>
2018-12-19 20:16:31 -0500

    updated

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
 (function () {
     'use strict';
     
-    angular.module('NameCalculator', [])
+   angular.module('NameCalculator',[])
+   .controller('NameCalculatorController', function ($scope) {
+       $scope.name = "";
+       $scope.totalValue = 0;
+
+       $scope.displayNumeric = function(){
+           var totalNameValue = calculateNumericorString($scope.name);
+           $scope.totalValue = totalNameValue;
+       };
+
+       function calculateNumericorString(string){
+           var totalStringValue = 0;
+           for(var i = 0; i < string.length; i++){
+                totalStringValue += string.charCodeAt(i);
+           }
+           return totalStringValue;
+       };
+
+   });
+
     
-    .controller('NameCalculatorController', function ($scope) {
-      $scope.name = "";
-      $scope.totalValue = 0;
-    
-      $scope.displayNumeric = function () {
-        var totalNameValue = calculateNumericForString($scope.name);
-        $scope.totalValue = totalNameValue;
-      };
-    
-    
-      function calculateNumericForString(string) {
-        var totalStringValue = 0;
-        for (var i = 0; i < string.length; i++) {
-          totalStringValue += string.charCodeAt(i);
-        }
-    
-        return totalStringValue;
-      }
-    
-    });
     
     
     })();
-    
